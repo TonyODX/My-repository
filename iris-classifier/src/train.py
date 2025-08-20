6d2d1d70b48820cf645b652317b535b88f3493e2
TonyODX <107518809+TonyODX@users.noreply.github.com>
2025-08-20 01:27:05 +0100

    Update train.py

--- iris-classifier/src/train.py
+++ iris-classifier/src/train.py
@@ -13,7 +13,7 @@ model = DecisionTreeClassifier(random_state=42)
 
 model.fit(X_train, y_train)
 
-joblib.dump(model, "iris-classfier/decision_tree_model.joblib");
+joblib.dump(model, "iris-classifier/outputs/decision_tree_model.joblib");
 loaded_model = joblib.load("decision_tree_model.joblib");
 
 y_pred = model.predict(X_test)
